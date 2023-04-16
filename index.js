a5bbb7d8de8a2e10d6b020180482675baa04f7e4
Altin Hysa <altinhysa84@gmail.com>
2023-04-16 22:02:20 +0200

    fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static('build'))
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
@@ -102,7 +104,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
