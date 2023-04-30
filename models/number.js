52a211d6ad8e387ab814af06bc97f4dfc6c0132d
Altin Hysa <altinhysa84@gmail.com>
2023-04-30 22:45:58 +0200

    asd

--- models/number.js
+++ models/number.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to ', url)
 
-mongoose.connect(url).then(result=> {
+mongoose.connect(url).then(result => {
     console.log('connected to db')
 }).catch((error) => {
     console.log('error connecting to db ', error.message)
